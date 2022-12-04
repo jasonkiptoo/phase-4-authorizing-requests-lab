5b48603f49377d5e57b8c3845c60eba1dd7c13a4
JasonKiptoo <kiptoojason@gmail.com>
2022-12-04 14:49:12 +0300

    completed assignment

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,7 @@ class ArticlesController < ApplicationController
   def index
     articles = Article.where(is_member_only: false).includes(:user).order(created_at: :desc)
     render json: articles, each_serializer: ArticleListSerializer
-  end
+  end 
 
   def show
     article = Article.find(params[:id])
